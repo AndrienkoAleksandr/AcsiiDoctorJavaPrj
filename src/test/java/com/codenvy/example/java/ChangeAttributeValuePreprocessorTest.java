073ecaf076af9da7da78a8a3a44fff79013e9edd
Oleksandr Andriienko <oandriie@redhat.com>
2017-10-25 01:49:48 +0300

    Next
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- src/test/java/com/codenvy/example/java/ChangeAttributeValuePreprocessorTest.java
+++ src/test/java/com/codenvy/example/java/ChangeAttributeValuePreprocessorTest.java
@@ -1,7 +1,6 @@
 package com.codenvy.example.java;
 
 import org.asciidoctor.Asciidoctor;
-import org.asciidoctor.OptionsBuilder;
 import org.asciidoctor.extension.JavaExtensionRegistry;
 import org.junit.Test;
 
@@ -12,37 +11,18 @@ import static org.junit.Assert.assertEquals;
 
 public class ChangeAttributeValuePreprocessorTest {
 
-    private static final String ASCII_DOC_HEADER = "= Sample Document\n" +
+    private static final String ASCII_DOC = "= Document Title\n" +
             "\n" +
-            "[style one]\n" +
-            "This is content of first content part\n" +
-            "\n" +
-            "[[partId]]\n" +
-            "[style two,role=partRole]\n" +
-            "--\n" +
-            "And content of second content part\n" +
-            "\n" +
-            "This block can be as long as you want.\n" +
-            "--";
+            "sample {content}";
 
     private Asciidoctor asciidoctor = create();
 
     @Test
     public void shouldParseWithoutPreprocessor() {
-        String outPut = asciidoctor.convert(ASCII_DOC_HEADER, new HashMap<>());
+        String outPut = asciidoctor.convert(ASCII_DOC, new HashMap<>());
 
         String expectedOutPut = "<div class=\"paragraph\">\n" +
-                "<p>This is content of first content part</p>\n" +
-                "</div>\n" +
-                "<div id=\"partId\" class=\"openblock style two partRole\">\n" +
-                "<div class=\"content\">\n" +
-                "<div class=\"paragraph\">\n" +
-                "<p>And content of second content part</p>\n" +
-                "</div>\n" +
-                "<div class=\"paragraph\">\n" +
-                "<p>This block can be as long as you want.</p>\n" +
-                "</div>\n" +
-                "</div>\n" +
+                "<p>sample {content}</p>\n" +
                 "</div>";
         assertEquals(expectedOutPut, outPut);
     }
@@ -52,22 +32,12 @@ public class ChangeAttributeValuePreprocessorTest {
         JavaExtensionRegistry extensionRegistry = asciidoctor.javaExtensionRegistry();
 
         extensionRegistry.preprocessor(ChangeAttributeValuePreprocessor.class);
-        String outPut = asciidoctor.convert(":homepage: http://asciidoctor.org\n" +
-                ":docslink: http://asciidoctor.org/docs[Asciidoctor's Docs]\n" +
-                ":desc: Asciidoctor is a mature, plain-text document format for +\n" +
-                "       writing notes, articles, documentation, books, and more. +\n" +
-                "       It's also a text processor & toolchain for translating +\n" +
-                "       documents into various output formats (i.e., backends), +\n" +
-                "       including HTML, DocBook, PDF and ePub.\n" +
-                ":checkedbox: pass:normal[`[&#10004;]`]\n" +
-                "\n" +
-                "Check out {homepage}[Asciidoctor]!\n" +
-                "\n" +
-                "{desc}\n" +
-                "\n" +
-                "Check out {docslink} too!\n" +
-                "\n" +
-                "{checkedbox} That's done!", new HashMap<>());
-        System.out.println(outPut);
+        String outPut = asciidoctor.convert(ASCII_DOC, new HashMap<>());
+
+        String expectedOutPut = "<div class=\"paragraph\">\n" +
+                "<p>sample Alex</p>\n" +
+                "</div>";
+
+        assertEquals(expectedOutPut, outPut);
     }
 }
